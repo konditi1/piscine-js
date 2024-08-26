6231de2b05129e1074c3585bd30abb724d218e97
fonditi <konditi1.com>
2024-08-26 17:35:52 +0300

    declaration

---
+++ declarations.js
@@ -0,0 +1,23 @@
+const escapeStr = "\` \\ \/ \" '"
+// console.log(escapeStr)
+
+const arr = [4, '2']
+// console.log(arr)
+
+const obj = {
+    str : "string",
+    num  : 85,
+    bool : false,
+    undef : undefined,
+}
+// console.log(obj)
+
+const nested = {
+    arr : [4, undefined, '2'],
+    obj : {
+        str : "string",
+        num : 4,
+        bool : true,
+    }
+}
+Object.freeze(nested, arr,obj)
