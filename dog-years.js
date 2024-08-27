0ce7b2caf207cb2124355a5c7a5c24e5754dead4
konditi1 <konditi1@gmail.com>
2024-08-27 23:13:31 +0300

    dog-years.js

--- dog-years.js
+++ dog-years.js
@@ -2,18 +2,18 @@ const dogYears = (planet, age) => {
     if (planet === "earth") {
         return parseFloat(((1 * age / 31557600) * 7).toFixed(2))  
     } else if (planet === "mercury") {
-       return parseFloat(((0.2408467 * age / 31557600) * 7).toFixed(2)) 
+       return parseFloat(( age / (0.2408467 * 31557600) * 7).toFixed(2)) 
     } else if (planet === "venus") {
-        return parseFloat(((0.61519726 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (0.61519726 * 31557600) * 7).toFixed(2))
     } else if (planet === "mars") {
-        return parseFloat(((1.8808158 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (1.8808158 * 31557600) * 7).toFixed(2))
     } else if (planet === "jupiter") {
-        return parseFloat(((11.862615 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (11.862615 * 31557600) * 7).toFixed(2))
     } else if (planet === "saturn") {
-        return parseFloat(((29.447498 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (29.447498 * 31557600) * 7).toFixed(2))
     } else if (planet === "uranus") {
-        return parseFloat(((84.016846 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (84.016846 * 31557600) * 7).toFixed(2))
     } else if (planet === "neptune") {
-        return parseFloat(((164.79132 * age / 31557600) * 7).toFixed(2))
+        return parseFloat((age / (164.79132 * 31557600) * 7).toFixed(2))
     }
 }
