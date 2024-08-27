59eccd3e00f97f9b5b02dc2440e3fa72adbd927b
fonditi <konditi1.com>
2024-08-27 12:44:40 +0300

    concat-str.js

--- concat-str.js
+++ concat-str.js
@@ -1,5 +1,5 @@
-const concatStr = (arg1, arg2) => arg1 + arg2
+const concatStr = (arg1, arg2) => new String(arg1) + new String(arg2)
 const concatStr2 = (arg1, arg2) => arg1.concat(arg2)
 
 
-// console.log(concatStr("hello", "world"))
+console.log(concatStr("hello", "world"))
