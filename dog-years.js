b81db2e6fd1cba8a1902338757bb3bbdfffa5695
konditi1 <konditi1@gmail.com>
2024-08-27 23:06:48 +0300

    dog-years.js

--- dog-years.js
+++ dog-years.js
@@ -1,19 +1,19 @@
 const dogYears = (planet, age) => {
-    if (planet === "Earth") {
+    if (planet === "earth") {
         return parseFloat(((1 * age / 31557600) * 7).toFixed(2))  
-    } else if (planet === "Mercury") {
+    } else if (planet === "mercury") {
        return parseFloat(((0.2408467 * age / 31557600) * 7).toFixed(2)) 
-    } else if (planet === "Venus") {
+    } else if (planet === "venus") {
         return parseFloat(((0.61519726 * age / 31557600) * 7).toFixed(2))
-    } else if (planet === "Mars") {
+    } else if (planet === "mars") {
         return parseFloat(((1.8808158 * age / 31557600) * 7).toFixed(2))
-    } else if (planet === "Jupiter") {
+    } else if (planet === "jupiter") {
         return parseFloat(((11.862615 * age / 31557600) * 7).toFixed(2))
-    } else if (planet === "Saturn") {
+    } else if (planet === "saturn") {
         return parseFloat(((29.447498 * age / 31557600) * 7).toFixed(2))
-    } else if (planet === "Uranus") {
+    } else if (planet === "uranus") {
         return parseFloat(((84.016846 * age / 31557600) * 7).toFixed(2))
-    } else if (planet === "Neptune") {
+    } else if (planet === "neptune") {
         return parseFloat(((164.79132 * age / 31557600) * 7).toFixed(2))
     }
 }
