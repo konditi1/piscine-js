e80309fdf0ff63e55d5ecd0f61c0ba909aa1b61e
fonditi <konditi1.com>
2024-08-27 12:29:19 +0300

    last-first-kiss.js

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -2,6 +2,6 @@ const first = (arg) => arg[0]
 
 const last = (arg) => arg[arg.length-1]
 
-const kiss = (arg) => [arg[0], arg[arg.length-1]]
+const kiss = (arg) => [arg[arg.length-1], arg[0]]
 
-// console.log(kiss("abc"))
+console.log(kiss("abc"))
