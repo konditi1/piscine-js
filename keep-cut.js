f7a0f1b7985eceee8986849fc3a31acb456660cd
konditi1 <konditi1@gmail.com>
2024-08-29 02:07:21 +0300

    keep-cut.js

--- keep-cut.js
+++ keep-cut.js
@@ -15,7 +15,13 @@ const keepFirst = str => str.slice(0, 2)
 const keepLast = str => str.slice(-2)
 
 const keepFirstLast = str => {
+    if (str.length <= 2) {
+        return str
+    }
    let  str1 = keepFirst(str)
     let str2 = keepLast(str)
+
     return str1 + str2
 }
+
+console.log(keepFirstLast('af'))
