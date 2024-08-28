ba35b29c66a49c774a1a12c485af57f359cbe5a1
konditi1 <konditi1@gmail.com>
2024-08-28 20:53:16 +0300

    is.js

--- is.js
+++ is.js
@@ -1,11 +1,11 @@
-is.num = num => typeof num === "number"
-is.nan = nan => Number.isNaN(nan)
-is.str = str => typeof str === "string"
-is.bool = bool => typeof bool === "boolean"
-is.undef = undef => typeof undef === "undefined"
-is.truthy = truthy => truthy ? true : false
-is.falsy = falsy => falsy ? true : false
-is.fun = fun => typeof fun === "function"
-is.def = def => def ? true : false
-is.arr = arr => Array.isArray(arr)
-is.obj = obj => typeof obj === "object" && obj !== null
+is.num = num => typeof num === "number";
+is.nan = nan => Number.isNaN(nan);
+is.str = str => typeof str === "string";
+is.bool = bool => typeof bool === "boolean";
+is.undef = undef => typeof undef === "undefined";
+is.truthy = truthy => truthy ? true : false;
+is.falsy = falsy => falsy ? true : false;
+is.fun = fun => typeof fun === "function";
+is.def = def => def ? true : false;
+is.arr = arr => Array.isArray(arr);
+is.obj = value => typeof value === "object" &&  value !== null;
