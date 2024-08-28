896132eadabfec49a4c0a8404af2e446d107bb84
konditi1 <konditi1@gmail.com>
2024-08-28 14:19:55 +0300

    sign.js

--- sign.js
+++ sign.js
@@ -2,3 +2,13 @@ const sign = num => { num > 0 ? 1
      : num < 0 ? -1 
      : 0;
     }
+
+const sameSign = (num1, num2) => {
+    if (num1 < 0 && num2 < 0) {
+        return true
+    } else if (num1 > 0 && num2 > 0) {
+        return true
+    } else {
+        return false
+    }
+}
