80c7ee89cd915d946ba8c56e18dd3ac40a0a1c30
konditi1 <konditi1@gmail.com>
2024-08-30 00:36:38 +0300

    get.js

--- get.js
+++ get.js
@@ -2,7 +2,13 @@ const get = (arr, path) => {
     let result = arr
     path = path.split(".")
     for (let i = 0; i < path.length; i++) {
+        if (result[path[i]] === undefined) {
+            return undefined
+        }
         result = result[path[i]]
     }
     return result
 }
+
+
+console.log(get({ nested: { key: 'value' } }, 'nx.nx'))
