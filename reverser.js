2614ac3f99568d90bb5dd5e3bf8386effd5c8861
konditi1 <konditi1@gmail.com>
2024-08-29 13:57:22 +0300

    reverser.js

--- reverser.js
+++ reverser.js
@@ -2,17 +2,19 @@ const reverse = arg => {
     if (Array.isArray(arg)) {
         let result = []
         for (const val of arg) {
-            result.push(val)
+            result.unshift(val)
         }
         return result
     }
 
     if (typeof arg === "string") {
-        let result = ""
+        let revStr = ""
         let i = arg.length -1
         for (;i >= 0; i--) {
-            result += arg[i]
+            revStr += arg[i]
         }
-        return result
+        return revStr
     }
 }
+
+console.log(reverse("1, 2, 3"))
