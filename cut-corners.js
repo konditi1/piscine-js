7bffe0de5242e8a61d7edc7ef10823f7dcd6435a
konditi1 <konditi1@gmail.com>
2024-08-29 05:22:35 +0300

    cut-corners.js

--- cut-corners.js
+++ cut-corners.js
@@ -1,67 +1,53 @@
 const round = num => {
-        let neg = false
-        if (num < 0) {
-            neg = true
-            num = -num
-        }
-
-        if (num - parseint(num) >= 0.5) {
-            num = parseint(num) + 1
-        } else {
-            num = parseint(num)
-        }
-
-        if (neg) {
-            return -num
-        }
-    return num
-}
-
-const ceil = num => {
-        if (num < 0) {
-            num = parseint(num)
-            return (num)
-        } else {
-            if (num - parseint(num) > 0) {
-                num = parseint(num) + 1
-            } else {
-                num = parseint(num)
-            }
-            return (num)
-        }
-}
-
-const floor = num => {
-    
-        if (num < 0) {
-        return (parseint(num) - 1)            
-        } else {
-            return (parseint(num))
-        }
+    let neg = false;
+    if (num < 0) {
+        neg = true;
+        num = -num;
     }
 
+    if (num - parseint(num) >= 0.5) {
+        num = parseint(num) + 1;
+    } else {
+        num = parseint(num);
+    }
 
-const trunc = num => parseint(num)
+    return neg ? -num : num;
+};
 
+const ceil = num => {
+    if (num < 0) {
+        return parseint(num);  // For negative numbers, ceil is the integer part
+    } else {
+        return (num - parseint(num) > 0) ? parseint(num) + 1 : parseint(num);
+    }
+};
 
-const parseint = num =>{
+const floor = num => {
+    if (num < 0) {
+        return (num === parseint(num)) ? num : parseint(num) - 1;  // Subtract 1 only if not already an integer
+    } else {
+        return parseint(num);
+    }
+};
+
+const trunc = num => parseint(num);
+
+const parseint = num => {
     let result = 0;
 
     // For positive numbers
-    if (num > 0) {
+    if (num >= 0) {
         while (num >= 1) {
             num -= 1;
             result += 1;
         }
     }
     // For negative numbers
-    else if (num < 0) {
+    else {
         while (num <= -1) {
             num += 1;
             result -= 1;
         }
     }
     return result;
-}
-
-console.log(ceil(-5.9))
+};
