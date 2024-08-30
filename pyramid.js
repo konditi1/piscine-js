ebe07b0ddaac885ea711f1d4d6e5753301c4ea81
konditi1 <konditi1@gmail.com>
2024-08-30 16:36:24 +0300

    pyramid.js

--- pyramid.js
+++ pyramid.js
@@ -1,16 +1,24 @@
 const pyramid = (str, num) => {
-    let result = "";
-    for(let row = 1; row <= num; row++) {
-        let spaces = ' '.repeat(num - row);
-        let stars = str.repeat(2 * row - 1);
-
-        result += spaces + stars
-
-        if (row < num) {
+    let result = ""
+    for (let i = 1; i <= num; i++) {
+        let temp = '';
+      
+        // Add spaces
+        for (let j = 1; j <= num - i; j++) {
+          temp += ' ';
+        }
+      
+        // Add stars
+        for (let k = 1; k <= 2 * i - 1; k++) {
+          temp += str;
+        }
+      
+        // Print the row
+        result += temp;
+        if (i < num) {
             result += "\n"
         }
-    }
-    return result
+      }
+      return result
 }
-
-console.log(pyramid('{}', 12))
+// console.log(pyramid('*', 5))
