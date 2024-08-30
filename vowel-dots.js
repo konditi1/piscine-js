d4c1838be5a027f23027fdec68a3e70255d2cccc
konditi1 <konditi1@gmail.com>
2024-08-31 02:01:43 +0300

    vowel-dots.js

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,6 +1,6 @@
+const vowels = /[aeiou]/gi
 const vowelDots = str => {
     let result = "";
-    let vowels = /[aeiou]/gi
     for (let i = 0; i < str.length; i++) {
         if (vowels.test(str[i])) {
             result += "."
