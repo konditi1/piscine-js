a57f737e56f61c01d922acf400a1c96e9b78cc61
konditi1 <konditi1@gmail.com>
2024-08-31 01:51:42 +0300

    letter-space-number.js

--- letter-space-number.js
+++ letter-space-number.js
@@ -3,6 +3,9 @@ const letterSpaceNumber = (str) => {
     let regex = /\b\d\b(?![a-zA-Z])/g
     
             result = str.match(regex)
+            if (result === null) {
+                return []
+            }
             for (let i = 0; i < result.length; i++) {
                 for (let j = 0; j < str.length; j++) {
                     if (str[j] === result[i]) {
@@ -14,4 +17,4 @@ const letterSpaceNumber = (str) => {
     return result
 }
 
-console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
+console.log(letterSpaceNumber('I like 7up.'))
