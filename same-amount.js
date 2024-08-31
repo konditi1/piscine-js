24b482ea18902836077386dec7676fd72e61879e
konditi1 <konditi1@gmail.com>
2024-08-31 03:25:18 +0300

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,12 +1,15 @@
 const sameAmount = (str, regEx1, regEx2) => {
-    let result = str.match(/regEx1/g)
+    let result = str.match(regEx1)[0]
     if (result === null) {
         return false
     }
-    let result2 = str.match(/regEx2/g)
+    let result2 = str.match(regEx2)[0]
     if (result2 === null) {
         return false
     }
     console.log(result, result2)
-    return result.length === result2.length
+
+    if (result === result2) {
+        return true
+    }
 }
