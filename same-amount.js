de00f6d2441558499fa70909507a77a939655746
konditi1 <konditi1@gmail.com>
2024-08-31 10:31:56 +0300

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -2,8 +2,9 @@ const sameAmount = (str, regEx1, regEx2) => {
     let result1 = str.match(regEx1) || []
     let result2 = str.match(regEx2) || []
 
-    // result1 = result1 === null ? [] : result1
-    // result2 = result2 === null ? [] : result2
+    if (result1.length === 0 && result2.length === 0) {
+        return true;
+    }
 
     
     return result1.length === result2.length
