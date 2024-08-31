1be61534f4d30b9aae688e5576194400fda0228f
konditi1 <konditi1@gmail.com>
2024-08-31 11:55:09 +0300

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,9 @@
 const sameAmount = (str, regEx1, regEx2) => {
-    const globalRegEx1 = new RegExp(regEx1.source, regEx1.flags.includes('g') ? regEx1.flags : regEx1.flags + 'g');
-    const globalRegEx2 = new RegExp(regEx2.source, regEx2.flags.includes('g') ? regEx2.flags : regEx2.flags + 'g')
+    // const globalRegEx1 = new RegExp(regEx1.source, regEx1.flags.includes('g') ? regEx1.flags : regEx1.flags + 'g');
+    // const globalRegEx2 = new RegExp(regEx2.source, regEx2.flags.includes('g') ? regEx2.flags : regEx2.flags + 'g')
+
+    const globalRegEx1 = new RegExp(regEx1, "g")
+    const globalRegEx2 = new RegExp(regEx2, "g")
     console.log(regEx1, regEx2)
 
     let result1 = str.match(globalRegEx1) || []
