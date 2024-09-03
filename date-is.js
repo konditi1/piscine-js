ce7418dad1ed39a586d5b2346b1948e10e1baa48
konditi1 <konditi1@gmail.com>
2024-09-03 21:53:29 +0300

    date-is.js

---
+++ date-is.js
@@ -0,0 +1,64 @@
+const isValid = date => {
+    let formatedDate = new Date(date)
+    if (formatedDate.toString() === "Invalid Date") {
+        return false
+    } else {
+        return true
+    }
+}
+
+const isAfter = (date1, date2) => {
+    if (!isValid(date1) || !isValid(date2)) {
+        return false
+    }
+
+    let formatedDate1 = new Date(date1)
+    let formatedDate2 = new Date(date2)
+
+    if (formatedDate1 > formatedDate2) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const isBefore = (date1, date2) => {
+    if (!isValid(date1) || !isValid(date2)) {
+        return false
+    }
+
+    let formatedDate1 = new Date(date1)
+    let formatedDate2 = new Date(date2)
+    
+    if (formatedDate1 < formatedDate2) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const isFuture = date => {
+    if (!isValid(date)) {
+        return false
+    }
+
+    let formatedDate = new Date(date)
+    if (formatedDate > new Date()) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const isPast = date => {
+    if (!isValid(date)) {
+        return false
+    }
+
+    let formatedDate = new Date(date)
+    if (formatedDate < new Date()) {
+        return true
+    } else {
+        return false
+    }
+}
