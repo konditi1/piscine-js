480f4cbbfb1cf8399e2de0022940d84c88dfc8fb
konditi1 <konditi1@gmail.com>
2024-09-04 01:00:56 +0300

    count-leap-years.js

---
+++ count-leap-years.js
@@ -0,0 +1,17 @@
+function countLeapYears(date) {
+    const year = date.getFullYear();
+    
+    let leapYearsCount = 0;
+
+    for (let y = 1; y <= year; y++) {
+        if (isLeapYear(y)) {
+            leapYearsCount++;
+        }
+    }
+
+    return leapYearsCount;
+}
+
+function isLeapYear(year) {
+    return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0);
+}
