fc4d5b17c390cb01c42d93a133f5ad70cdc08c67
konditi1 <konditi1@gmail.com>
2024-09-05 23:22:09 +0300

    quantifiers.js

---
+++ quantifiers.js
@@ -0,0 +1,27 @@
+function every(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i], i, arr)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function some(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function none(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return true;
+        }
+    }
+    return false;
+}
