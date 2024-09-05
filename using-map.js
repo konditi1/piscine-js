5b30f5102dc30e0c217cd4498e34386f4ce1aee6
konditi1 <konditi1@gmail.com>
2024-09-05 22:44:05 +0300

    using-map.js

--- using-map.js
+++ using-map.js
@@ -13,7 +13,7 @@ const upperCasingStates = states => states.map(state => {
 const fahrenheitToCelsius = arr => arr.map(val => {
     let splitVal = val.split("°")
     let num = Number(splitVal[0])
-    return (Math.floor((num - 32)/1.8))    
+    return (Math.floor((num - 32)/1.8) + "°C")    
 })
 
 const trimTemp = temps => temps.map(temp => {
@@ -21,9 +21,20 @@ const trimTemp = temps => temps.map(temp => {
     return {...temp, temperature: trimed }  //  city: temp.city
 })
 
-const tempForecasts = arr => arr.map ()
+const tempForecasts = (arr) => {
+    return arr.map(({ temperature, city, state }) => {
+        return `${celsius(temperature)} in ${city}, ${caps(state)}`;
+    });
+};
 
-console.log(trimTemp([
-    { city: 'Los Angeles', temperature: '  101 °F   ' },
-    { city: 'San Francisco', temperature: ' 84 ° F   ' },
-  ]))
+const celsius = (temp) => {
+    const fahrenheit = parseInt(temp, 10);
+    const celsiusValue = Math.floor((fahrenheit - 32) * (5/9));
+    return `${celsiusValue}°Celsius`;
+};
+
+const caps = (str) => {
+    return str.split(' ')
+              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(' ');
+};
