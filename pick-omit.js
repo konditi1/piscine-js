7539eeb8f76a880f57df4fc803ed66f8b96b8061
konditi1 <konditi1@gmail.com>
2024-09-09 18:54:10 +0300

    pick-omit.js

---
+++ pick-omit.js
@@ -0,0 +1,22 @@
+const pick = (obj, keys) => {
+    let newObj = {}
+    for (let key of keys) {
+        if (obj.hasOwnProperty(key)) {
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
+}
+
+const omit = (obj, keys) => {
+    let newObj = {}
+    for (let key in obj) {
+        if (!keys.includes(key)) {
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
+}
+
+console.log(pick({ a: 1, b: 2, c: 3 }, "acb"))
+console.log(omit({ a: 1, b: 2, c: 3 }, 'acb'))
