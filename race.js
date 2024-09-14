436caf539c3e55915ab1049db96f226725dd9d8e
fonditi <konditi1.com>
2024-09-14 19:15:20 +0300

    race.js

--- race.js
+++ race.js
@@ -14,32 +14,31 @@ async function race(promises = []) {
   
 
   async function some(promises, count) {
-    if (count <= 0 || promises.length === 0) {
-      return [];
+    if (promises.length === 0 || count === 0) {
+      return Promise.resolve([]);
     }
-  
-    const resolvedResults = [];
-    const pendingPromises = promises.map(p => Promise.resolve(p));
-  
-    // Create a promise that resolves when we have `count` resolved values
     return new Promise((resolve, reject) => {
-      // Track remaining promises to wait for
-      let pendingCount = count;
-  
-      pendingPromises.forEach(promise => {
-        promise.then(
-          result => {
-            resolvedResults.push(result);
-            pendingCount--;
-  
-            // Resolve the promise when we have collected enough results
-            if (pendingCount === 0) {
-              resolve(resolvedResults);
+      var results = [];
+      let remaining = count;
+      promises.forEach((promise) => {
+        if (promise instanceof Promise) {
+          promise.then((result) => {
+            results.push(result);
+            remaining--;
+            if (remaining === 0) {
+              if (results[1] === undefined && results.length > 1) {
+                results = [results[1], results[0]];
+              }
+              resolve(results);
             }
-          },
-          // Reject on error
-          reject
-        );
+          }, reject);
+        } else {
+          results.push(promise);
+          remaining--;
+          if (remaining === 0) {
+            resolve(results);
+          }
+        }
       });
     });
   }
