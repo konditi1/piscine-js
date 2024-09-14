c6043ee8bbe20a0c9350d662b2321a12d9d4720c
fonditi <konditi1.com>
2024-09-14 17:52:56 +0300

    keep-trying-or-giveup.js

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,3 +1,20 @@
+function retry(count, callback) {
+    return async function (...args) {
+      let attempts = 0;
+  
+      while (attempts <= count) {
+        try {
+          return await callback(...args);
+        } catch (error) {
+          attempts++;
+          if (attempts > count) {
+            throw new Error('Max retries exceeded');
+          }
+        }
+      }
+    };
+  }
+
 function timeout(delay, callback) {
     return async function (...args) {
       let timedOut = false;
