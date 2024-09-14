d2d21cf408bcdfefd099a314423ee4a28c18a49c
fonditi <konditi1.com>
2024-09-14 18:55:20 +0300

    race.js

--- race.js
+++ race.js
@@ -1,27 +1,31 @@
-function race(promises) {
+async function race(promises) {
     return new Promise((resolve, reject) => {
-      for (const promise of promises) {
-        Promise.resolve(promise).then(resolve, reject);
+      if (promises.length === 0) {
+        return resolve(); // Resolve immediately if no promises are provided
       }
+      promises.forEach(promise => {
+        Promise.resolve(promise).then(resolve, reject);
+      });
     });
   }
+  
 
-  async function some(promises, count) {
+async function some(promises, count) {
     if (count <= 0 || promises.length === 0) {
       return [];
     }
   
     const resolvedValues = [];
-    const pendingPromises = promises.map(promise => Promise.resolve(promise));
+    const pendingPromises = promises.map(p => Promise.resolve(p));
   
     while (resolvedValues.length < count && pendingPromises.length > 0) {
       try {
         const result = await Promise.race(pendingPromises);
         resolvedValues.push(result);
         // Remove the resolved promise from pendingPromises
-        pendingPromises = pendingPromises.filter(p => p !== Promise.resolve(result));
+        pendingPromises.splice(pendingPromises.indexOf(Promise.resolve(result)), 1);
       } catch (error) {
-        // Handle errors if needed
+            return new error
       }
     }
   
