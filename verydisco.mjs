8ca3c41997c024794af0d66f5e184dc75867ec77
konditi1 <konditi1@gmail.com>
2024-09-17 19:01:40 +0300

    verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,21 +1,13 @@
-import { argv } from "node:process";
+const word = process.argv
+let args = word[2]
+let argsArr = str.split(' ')
 
-const veryDisco = (arg) => {
-    let half = Math.ceil(arg.length / 2);
-    return arg.slice(half) + arg.slice(0, half);
-};
-
-if (argv.length > 2) {
-    let str = argv.slice(2).join(" "); 
-    let arr = str.split(" "); 
-
-    for (let i in arr) {
-        let w = arr[i];
-        let half = Math.ceil(w.length / 2);
-        let first = w.slice(0, half);
-        let last = w.slice(half);
-        arr[i] = last + first;
-    }
-
-    console.log(arr.join(" "));
+for(let i in argsArr) {
+    let arg = argsArr[i]
+    let end = (Math.ceil(arg.length/2))
+    let first = arg.slice(0,end)
+    let last = arg.slice(end)
+    argsArr[i] = last + first
 }
+
+console.log(argsArr.join(' '))
