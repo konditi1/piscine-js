f5fc0e59bec4cb946ca1f0396f435c66e3474f0c
konditi1 <konditi1@gmail.com>
2024-09-17 19:54:17 +0300

    verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,13 +1,11 @@
-const word = process.argv
-let args = word[2]
+const args = process.argv[2]
 let argsArr = args.split(' ')
 
-for(let i in argsArr) {
-    let arg = argsArr[i]
+argsArr.forEach((arg, index) => {
     let end = (Math.ceil(arg.length/2))
     let first = arg.slice(0,end)
     let last = arg.slice(end)
-    argsArr[i] = last + first
-}
+    argsArr[index] = last + first
 
-console.log(argsArr.join(' '))
+    console.log(argsArr.join(' '))
+})
