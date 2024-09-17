00bdd2be58fcb28befd9abe485ee8f3b639a29da
konditi1 <konditi1@gmail.com>
2024-09-18 01:02:16 +0300

    add: support files

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,10 +1,13 @@
-const args = process.argv[2]
-let argsArr = args.split(' ')
 
-argsArr.forEach((arg, index) => {
+const verydisco = () => {
+    const args = process.argv[2]
+    let argsArr = args.split(' ')
+    argsArr.forEach((arg, index) => {
     let end = (Math.ceil(arg.length/2))
     let first = arg.slice(0,end)
     let last = arg.slice(end)
     argsArr[index] = last + first
 })
-console.log(argsArr.join(' '))
+return argsArr.join(' ')
+}
+console.log(verydisco())
